61a03bda1310e738b3d719794ac6ed23086c4ce0
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2018-04-26 15:12:05 -0500

    'removed_image_size_method_in_item'

--- app/models/item.rb
+++ app/models/item.rb
@@ -23,9 +23,9 @@ class Item < ApplicationRecord
     #     end 
     # end 
 
-    def image_size 
-        if image.size > 5.megabytes 
-            errors.add(:image, "should be less than 5MB")
-        end 
-    end
+    # def image_size 
+    #     if image.size > 5.megabytes 
+    #         errors.add(:image, "should be less than 5MB")
+    #     end 
+    # end
 end
