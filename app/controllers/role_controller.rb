946aa433d7782de0ba4a0c26ebee880ff31065c6
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2018-04-26 08:19:37 -0500

    'role_index_new_form'

--- app/controllers/role_controller.rb
+++ app/controllers/role_controller.rb
@@ -1,8 +1,24 @@
 class RoleController < ApplicationController
+    def index 
+        @roles = Role.all 
+    end 
+    def show 
+        @role = Role.find(params[:id])
+    end 
     def new
         #As a visitor I want to be able to 
         #purchase a subscription
         #just ask for first name, last name and email 
         @role = Role.new 
+    end
+    def create 
+        binding.pry
+        if @role.save
+            flash[:notice] = "new role created"
+            redirect_to @role 
+        else 
+            flash[:alert] = "new role has not been created"
+            render "new"
+        end 
     end
 end
