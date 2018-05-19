c9950f9a2c2157afdd94b2beb364f3541fb3c8d1
github username <github email address>
2018-05-19 17:42:50 -0500

    making it better

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
     before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
     before_action :correct_user, only: [:edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :set_s3_direct_post, only: [:new, :edit, :create, :update]
+
     #before_action :set_subscription
     before_action :admin_user, only: :destroy
     #https://stackoverflow.com/questions/23585871/implementing-multiple-user-roles
@@ -70,7 +73,13 @@ class UsersController < ApplicationController
     def admin_user 
         redirect_to(root_url) unless current_user.admin?
     end
-
+# Use callbacks to share common setup or constraints between actions.
+    def set_user
+    @user = User.find(params[:id])
+    end
+    def set_s3_direct_post
+        @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+    end
     # def set_subscription 
     #     @subscription = Subscription.find(params[:subscription_id])
     # end
