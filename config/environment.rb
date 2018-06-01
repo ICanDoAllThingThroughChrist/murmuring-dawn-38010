abd741b9e396f80a8542e6136e9a060bac6afbd0
github username <github email address>
2018-05-31 19:16:34 -0500

    added-config-for-heroku-in-production-development-rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.default_url_options = { :host => 'murmuring-dawn-38010.heroku.com' }
+
