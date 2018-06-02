18530f738e505597edd1521f7934631036bca7f5
github username <github email address>
2018-06-01 22:19:43 -0500

    removed orphaned attributes from item params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -58,7 +58,8 @@ class ItemsController < ApplicationController
 
 private
     def item_params
-        params.require(:item).permit(:title,:description,:image,:size,:URL,:image_cache,:remove_image,box_ids:[],:avatar)
+        params.require(:item).permit(:title, :description, :URL, :avatar, box_ids:[])
+        params.require(:friend).permit(:avatar, :name)
     end
 #As an administrator I want to be able to 
 # add a new item to a box"=>Admin::BoxItemsController#New;
