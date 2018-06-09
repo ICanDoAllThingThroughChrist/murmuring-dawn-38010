5e2221e245ec1bb31ab22c013aa668fb11cea579
github username <github email address>
2018-06-09 17:32:34 -0500

    removed-updated-created-at-boxes-controller

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -132,7 +132,6 @@ private
         :shipped, :order_id,
         :user_id, :subscription_level,:month,
         :year,:title, item_ids:[],
-        items_attributes: [:title], :created_at,
-        :updated_at)
+        items_attributes: [:title])
     end
 end
