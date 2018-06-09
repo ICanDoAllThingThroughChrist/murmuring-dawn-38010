f56b6ce6f82081545f38ce9021845b667c8c5815
github username <github email address>
2018-06-09 11:41:49 -0500

    Demo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,7 +78,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     end
     def set_s3_direct_post
-        @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
+        @s3_direct_post = S3_BUCKET_NAME.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
     end
     # def set_subscription 
     #     @subscription = Subscription.find(params[:subscription_id])
